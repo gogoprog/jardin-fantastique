7cca027f8ee302805174e0786caa18f227d370a1
Gauthier Billot <gogoprog@gmail.com>
2020-03-28 13:16:43 +0100

    wip

--- src/game/Factory.hx
+++ src/game/Factory.hx
@@ -61,35 +61,6 @@ class Factory {
         return e;
     }
 
-    static public function createEnemy() {
-        var e = new Entity();
-        var sprite = new Sprite("guy");
-        e.add(sprite);
-        sprite.setOrigin(0.5, 0.5);
-        e.add(new Transform());
-        e.get(Transform).position.y = 100;
-        e.get(Transform).position.x = 300;
-        // sprite.animations.add("idle", untyped [0]);
-        // sprite.animations.add("walk", untyped [0, 1]);
-        // sprite.animations.add("jump", untyped [0, 1]);
-        // sprite.animations.play('walk', 5, true);
-        var sprite = e.get(Sprite);
-        whiplash.Lib.phaserScene.physics.add.existing(sprite);
-        untyped sprite.body.collideWorldBounds = true;
-        untyped sprite.body.setSize(8, 15);
-        untyped sprite.body.offset.setTo(4, 0);
-        untyped sprite.body.x = 500;
-        return e;
-    }
-
-    static public function createFlower() {
-        var e = new Entity();
-        e.add(new Sprite("flower"));
-        e.add(new Transform());
-        e.add(new Flower());
-        return e;
-    }
-
     static public function createCamera() {
         var e = new Entity();
         e.add(new Transform());
